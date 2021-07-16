81df69d58e19924aa72b87356a7c097dbb417cb8
adarsh_divergentsoftlab <adarsh.singh@divergentsl.com>
2021-07-16 11:35:01 +0530

    upadating our application, replaced cart selector with memorized selector

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,25 +1,28 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart  } from './cart.util';
+import { addItemToCart } from './cart.util';
+
+
 
 const INITIAL_STATE = {
-    hidden: true,
-    cartItIems:[]
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            };
-            case CartActionTypes.ADD_ITEM:
-            return{
-                ...state,
-                cartItIems: addItemToCart(state.cartItIems, action.payload)
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    default:
+      return state;
+  }
 };
+
 export default cartReducer;
